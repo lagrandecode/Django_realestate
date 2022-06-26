c1539b4f912fe8befe862b8d16141426af8ae0e6
lagrandecode <seunogunmolufirst1@gmail.com>
2022-06-26 12:29:11 +0100

    "commit"

--- listings/views.py
+++ listings/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Listings
 
 # Create your views here.
+def listing_list(request):
+
+    listing = Listings.objects.all()
+    context = {
+        "listing" : listing
+    }
+    return render(request, "lisiting.html", context)
