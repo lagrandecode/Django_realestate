86294be39eeb556b31f9300a494fdaf351f9ad07
lagrandecode <seunogunmolufirst1@gmail.com>
2022-06-26 09:00:18 +0100

    "commit"

--- listings/admin.py
+++ listings/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Listings
 
 # Register your models here.
+
+
+admin.site.register(Listings)
--- listings/models.py
+++ listings/models.py
@@ -1,3 +1,20 @@
+
+
 from django.db import models
 
 # Create your models here.
+
+class Listings(models.Model):
+    title = models.CharField(max_length=250)
+    price = models.IntegerField()
+    house_no = models.IntegerField()
+    square_metre = models.CharField(max_length=250)
+    description = models.CharField(max_length=250)
+    address = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.title
+
+   
+
+ 
